bcb08c723fd006d01060574e5eb70ae032868f07
Axel GALLIC <axel.gallic@orange.fr>
2025-05-02 18:11:33 +0200

    Ajout du projet & premier commit

---
+++ historique-presse-papiers@axaul/applet.js
@@ -0,0 +1,27 @@
+const Applet = imports.ui.applet;
+const Util = imports.misc.util;
+const Main = imports.ui.main;
+
+function HistoriquePressePapiers(orientation, panelHeight, instanceId) {
+    this._init(orientation, panelHeight, instanceId);
+}
+
+HistoriquePressePapiers.prototype = {
+    __proto__: Applet.IconApplet.prototype,
+
+    _init: function(orientation, panelHeight, instanceId) {
+        Applet.IconApplet.prototype._init.call(this, orientation, panelHeight, instanceId);
+
+        this.set_applet_icon_name("historique-presse-papiers");
+        this.set_applet_tooltip("Ouvrir l'historique du presse-papiers");
+    },
+
+    on_applet_clicked: function() {
+        // pour l'instant j'envoie juste une notification
+        Main.notify("Hitorique du presse-papiers", "Applet cliqué.");
+    }
+};
+
+function main(metadata, orientation, panelHeight, instanceId) {
+    return new HistoriquePressePapiers(orientation, panelHeight, instanceId);
+}
